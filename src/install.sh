b25f7f05e162132dd56e73b87afd2a9dbee3b9fe
Dustin Blackman <dustinblackman@users.noreply.github.com>
2015-06-25 18:45:45 -0400

    output musicbrainz

--- src/install.sh
+++ src/install.sh
@@ -203,7 +203,8 @@ cat <<'EOT' > /etc/my_init.d/004-import-databases--and-or-run-everything.sh
 #!/bin/bash
 crontab /root/cronjob
 cd /opt/musicbrainz
-plackup -Ilib -r > /dev/null 2>&1 &
+echo "- STARTING MusicBrainz"
+plackup -Ilib -r
 EOT
 
 
